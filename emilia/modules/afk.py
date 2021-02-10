fde8ef7a6d642eb7db749b04f0ac570d933c7d66
ronaldyganteng <77110439+ronaldyganteng@users.noreply.github.com>
2021-02-10 08:41:00 +0000

    Update afk.py

--- emilia/modules/afk.py
+++ emilia/modules/afk.py
@@ -30,7 +30,7 @@ def afk(bot: Bot, update: Update):
         reason = ""
 
     sql.set_afk(update.effective_user.id, reason)
-    send_message(update.effective_message, tl(update.effective_message, "{} sekarang AFK!").format(update.effective_user.first_name))
+    send_message(update.effective_message, tl(update.effective_message, "{} Dia Sekarang Sudah Meninggoy!! 😭").format(update.effective_user.first_name))
 
 
 @run_async
@@ -42,7 +42,7 @@ def no_longer_afk(bot: Bot, update: Update):
 
     res = sql.rm_afk(user.id)
     if res:
-        send_message(update.effective_message, tl(update.effective_message, "{} sudah tidak AFK!").format(update.effective_user.first_name))
+        send_message(update.effective_message, tl(update.effective_message, "{} Ehh Dia Hidup Lagi, Kenapa Lu Mesti Hidup Lagi Sii! 😭😂!").format(update.effective_user.first_name))
 
 
 @run_async
